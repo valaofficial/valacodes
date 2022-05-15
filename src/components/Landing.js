1d1808168079ca580b2c7947234be0829a6d1fd5
Maina Hyelavala <mhyelavala@gmail.com>
2022-05-16 00:45:55 +0100

    Updates

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Landing.css"
 import Navbar from './Navbar'
+import data from  './projects.json'
 
+import { Link, animateScroll as scroll } from "react-scroll";
 function Landing() {
+  console.log(data.projects)
   return (
     <section id="SiteSection">
         <Navbar/>
@@ -10,6 +13,7 @@ function Landing() {
         <About/>
         <Skills/>
         <MyProjects/>
+        <Contact/>
     </section>
   );
 }
@@ -27,11 +31,11 @@ function IntroSection(){
             <h4>& I’m a Developer . Designer</h4>
             <p>I’m a Nigerian based front-end developer
                 who specializes in building beautiful & interactive
-                front-end experiences for you  
+                user experiences for you  
             </p>
 
             <button className='intro-cta'>
-              Hire Me
+              Let's Build Stuff
             </button>
           </div>
 
@@ -41,7 +45,9 @@ function IntroSection(){
         </div>
 
         <div className='downBtn'>
-        <img src='/images/Down.png' alt='Down'/>
+          <Link spy={true} smooth={true} duration={300} to='about-section'>
+            <img src='/images/Down.png' alt='Down'/>
+          </Link>
         </div>
       </section>
     </>
@@ -55,11 +61,11 @@ function About(){
         <p>
         Hello! My name is Hyelavala and I enjoy creating  awesome
         user interfaces using my skills, I started my journey in 2019 when i attended
-        my first Hackathon
+        my first Hackathon and fell in love with the joys of coding and building things
         </p>
 
         <p>
-        Since then i’ve worked on alot of  personal & freelance
+        Since then i’ve worked on a few personal & freelance
         projects as well as getting my first internship at a 
         Software development firm in 2020
         </p>
@@ -103,9 +109,13 @@ function Skills(){
               <p>Git</p>
             </li>
             <li>
+              <img src='/images/tailwind.png' alt='Tailwind'/>
+              <p>Tailwind</p>
+            </li>
+            {/* <li>
               <img src='/images/image 4.png' alt='Nodejs'/>
               <p>NodeJs</p>
-            </li>
+            </li> */}
           </ul>
         </div>
 
@@ -138,8 +148,62 @@ function Skills(){
 function MyProjects(){
   return(
     <section id="projects-section">
-      
+
+      <h1>My Projects</h1>
+      {data.projects?
+      <div className='project-card-div'>
+        {data.projects.map(project =>(
+          <div className='project-card'>
+            <div className='project-img'>
+              <img src='/images/folder.png' alt='Folder'/>
+            </div>
+            <h3 className='project-title'>{project.title}</h3>
+            <p className='project-desc'>{project.desc}</p>
+            <div className='project-stack'>
+              {project.tech_stack.map(lang =>(
+              <h5>{lang}</h5>
+              ))}
+            </div>
+            <div className='project-link-div'>
+              <a className='project-link' href={project.link}>Visit</a>
+            </div>
+          </div>
+        ))}
+      </div>
+      :null}
     </section>
   )
 }
 
+
+
+function Contact(){
+  return(
+    <section id="contact-section">
+      <h1 className='contact-head'>Get In Touch</h1>
+      <h3 className='contact-msg'>I'm currently open to working with various teams of creative designers and developers.
+        <br/>
+        If you think we might be a good fit for one another
+      </h3>
+      
+      <div className='contact-btn'>
+        <a href='mailto: mhyelavala@gmail.com'>Message Me</a>
+      </div>
+
+      <h5 className='contact-online'>Contact me online</h5>
+      <div className='contact-social-div'>
+      <a className='contact-social' href='https://github.com/valaofficial'>
+        <img src='/images/github.png' alt='Github'/>
+      </a>
+      <a className='contact-social' href='https://www.linkedin.com/in/maina-hyelavala-99b08a129/'>
+      <img src='/images/linkedin.png' alt='Linkedin'/>
+      </a>
+      <a className='contact-social'href='https://twitter.com/itz_vala'>
+      <img src='/images/twitter.png' alt='Twitter'/>
+      </a>
+      </div>
+      <hr/>
+      <p className='footer-txt'>Designed & Developed by Maina Hyelavala</p>
+    </section>
+  )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,99 @@
 import React from 'react';
 import './Navbar.css'
-
+import { Link, animateScroll as scroll } from "react-scroll";
+import 'antd/dist/antd.css';
+import { Menu, Dropdown } from 'antd';
 function Navbar() {
+  let width = window.screen.width;
     return (
       <nav>
           <div className='navImg'>
+            <Link spy={true} smooth={true} duration={300} to='intro-section'>
               <img src='/images/logo.png' alt='Logo'/>
+            </Link>
           </div>
 
+          {width > 768 ?
           <div className='navbar-links'>
-              <ul>
-                  <li>
-                      About
-                  </li>
-                  <li>
-                      My Skills
-                  </li>
-                  <li>
-                      My Projects
-                  </li>
-                  <li>
-                      Contact
-                  </li>
-                  <div className='resume-btn'>
-                      My Resume
-                  </div>
-              </ul>
-          </div>
+          <ul>
+              <li>
+                <Link spy={true} smooth={true} duration={300} to='about-section'>
+                    About
+                  </Link>
+              </li>
+              <li>
+                <Link spy={true} smooth={true} duration={300} to='skills-section'>
+                    My Skills
+                </Link>
+              </li>
+              <li>
+                  <Link spy={true} smooth={true} duration={300} to='projects-section'>
+                    My Projects
+                  </Link>
+              </li>
+              <li>
+                  <Link spy={true} smooth={true} duration={300} to='contact-section'>
+                    Contact
+                  </Link>
+              </li>
+              <a href='/files/resume.pdf'  download='Maina Hyelavala Resume'>
+                <div className='resume-btn'> My Resume </div>
+              </a>
+          </ul>
+      </div>
+      :<MobileNav/>}
       </nav>
     );
   }
 
 export default Navbar;
+
+function MobileNav(){
+const menu = (
+  <Menu
+    items={[
+      {
+        label: <Link spy={true} smooth={true} duration={300} to='about-section'> About </Link>,
+        key: '0',
+      },{
+        type: 'divider',
+      },
+      {
+        label: <Link spy={true} smooth={true} duration={300} to='skills-section'> My Skills </Link>,
+        key: '1',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        label: <Link spy={true} smooth={true} duration={300} to='projects-section'> My Projects </Link>,
+        key: '2',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        label: <Link spy={true} smooth={true} duration={300} to='contact-section'> Contact </Link>,
+        key: '3',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        label: <a href='/files/resume.pdf'  download='Maina Hyelavala Resume'><div className='resume-btn'> My Resume </div></a>,
+        key: '2',
+      }
+    ]}
+  />
+);
+
+return  (
+  <Dropdown  overlay={menu} trigger={['click']}>
+    <a onClick={(e) => e.preventDefault()}>
+      <div className='menu-icon'>
+        <i class="fi fi-sr-apps"></i>
+      </div>
+    </a>
+  </Dropdown>
+);
+}
